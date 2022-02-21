0f3fe0722aba574b397d230a8cb361537481907c
Vasiliy <vakawelli@gmail.com>
2022-02-21 20:35:54 +0200

    added 2 ways to find the most frequent number in array

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,9 +4,63 @@ namespace Task1
 {
     class Program
     {
+
+        static int findFrequentElement1(int[] nums)
+        {
+            int number = 0;
+            int maxFrequent = 0;
+            int frequent = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] == nums[i - 1])
+                {
+                    frequent++;
+                }
+                else
+                {
+                    frequent = 1;
+                }
+
+                if (frequent > maxFrequent)
+                {
+                    maxFrequent = frequent;
+                    number = nums[i-1];
+                }
+            }
+
+            return number;
+        }
+        static int findFrequentElement2(int[] nums)
+        {
+            int number = 0;
+            int maxFrequent = 0;
+            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int frequent = 0;
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        frequent++;
+                    }
+
+                    if (frequent > maxFrequent)
+                    {
+                        maxFrequent = frequent;
+                        number = nums[i];
+                    }
+                }
+            }
+                return number;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = new int[] { 1, 2, 2, 2, 2, 5, 5, 6, 7, 8 };
+            
+
+            Console.WriteLine(findFrequentElement2(numbers));
         }
     }
 }
