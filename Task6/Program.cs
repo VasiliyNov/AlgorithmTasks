2a8d6c94f2a114a20b2bcdb5f6f352d442ace8b7
Vasiliy <vakawelli@gmail.com>
2022-03-03 11:27:43 +0200

    added project Task6 and CheckConsistentStrings method in it

---
+++ Task6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Task6
+{
+    class Program
+    {
+        private static void CheckConsistentStrings(string allowed, string[] masWords)
+        {
+            for (int i = 0; i < masWords.Length; i++)
+            {
+                if (!masWords[i].Any((el) => !allowed.Contains(el)))
+                {
+                    Console.WriteLine(masWords[i]);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string allowed = "ab";
+            string[] words = new[] { "a", "b", "c", "ab", "ac", "bc", "abc" };
+
+            CheckConsistentStrings(allowed, words);
+        }
+    }
+}
