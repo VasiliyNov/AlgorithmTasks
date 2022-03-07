6b4a67a036006e9ae4d5be4951a46711944e4611
Vasiliy <vakawelli@gmail.com>
2022-03-07 23:26:39 +0200

    created method GetMountainPeakIndex() which finds the index of the largest number (top of the mountain) in the array

---
+++ Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Task8
+{
+    class Program
+    {
+        private static int GetMountainPeakIndex(int[] array)
+        {
+            return Array.IndexOf(array, array.Max());
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = new[] { 24, 69, 100, 99, 79, 78, 67, 36, 26, 19 };
+
+            Console.WriteLine(GetMountainPeakIndex(arr));
+        }
+    }
+}
