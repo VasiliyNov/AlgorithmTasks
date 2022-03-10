f41e74e576a21ba0e50bdfab8a84670f698a8595
Vasiliy <vakawelli@gmail.com>
2022-03-10 13:50:47 +0200

    added a method ReverseOfCharactersInArray() that reverses the order of characters in a character array

---
+++ Task10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task10
+{
+    class Program
+    {
+        private static void ReverseOfCharactersInArray(char[] charStr)
+        {
+            if (1 <= charStr.Length && charStr.Length <= 105)
+            {
+                int i = 0;
+                int j = charStr.Length - 1;
+
+                while (i < j)
+                {
+                    (charStr[i], charStr[j]) = (charStr[j], charStr[i]);
+                    i++;
+                    j--;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            char[] s = new char[] { 'h', 'e', 'l', 'l', 'o' };
+
+            ReverseOfCharactersInArray(s);
+            Console.WriteLine(s);
+        }
+    }
+}
