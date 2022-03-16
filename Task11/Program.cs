9b0af1f72fb79e05f242816e93fe4b8f8fee3635
Vasiliy <vakawelli@gmail.com>
2022-03-16 13:44:27 +0200

    Added the GetLuckyInteger() method that looks for an integer which has a frequency in the array is equal to its value.

---
+++ Task11/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task11
+{
+    class Program
+    {
+        private static int GetLuckyInteger(int[] arr)
+        {
+            Dictionary<int, int> luckyIntegers = new();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!luckyIntegers.ContainsKey(arr[i]))
+                {
+                    luckyIntegers.Add(arr[i], 1);
+                }
+                else
+                {
+                    luckyIntegers[arr[i]]++;
+                }
+            }
+
+            luckyIntegers = new Dictionary<int, int>(luckyIntegers.Where(l => l.Key == l.Value));
+
+            if (luckyIntegers.Count > 0)
+            {
+                return luckyIntegers.Values.Max();
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr1 = new[] { 1, 2, 2, 3, 3, 3 };// 3
+            Console.WriteLine(GetLuckyInteger(arr1));
+
+            int[] arr2 = new[] { 2, 2, 3, 4 };// 2
+            Console.WriteLine(GetLuckyInteger(arr2));
+
+            int[] arr3 = new[] { 2, 2, 2, 3, 3 };// -1, there are no lucky numbers
+            Console.WriteLine(GetLuckyInteger(arr3));
+        }
+    }
+}
