370e53f2c5bb19f894b2ba9d0b528f30df37ea46
Vasiliy <vakawelli@gmail.com>
2022-03-21 17:55:39 +0200

    Created the classes Node and MyList. And also added method GetMiddleNode() which returns the middle node from a singly linked list.

---
+++ Task12/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    class Node<T>
+    {
+        public Node(T value, Node<T> next = null)
+        {
+            Data = value;
+            Next = next;
+        }
+        public Node() { }
+
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+    }
+}
---
+++ Task12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task12
+{
+    class Program
+    {
+        private static Node<T> GetMiddleNode<T>(MyList<T> list)
+        {
+            Node<T> current = list.First;
+
+            for (int i = 0; i < list.Count / 2; i++)
+            {
+                current = current.Next;
+            }
+
+            return current;
+        }
+
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new(new int[] { 1, 2, 3, 4, 5, 6 });
+
+            Node<int> middleNode = GetMiddleNode(myList);
+
+            Console.WriteLine($"middleNode.Value = {middleNode.Data}");
+
+            while (middleNode.Next != null)
+            {
+                middleNode = middleNode.Next;
+                Console.WriteLine(middleNode.Data);
+            }
+        }
+    }
+}
