27d0346e7eb150899397d6cdc6368f9f85f16be7
Vasiliy <vakawelli@gmail.com>
2022-04-19 17:28:48 +0300

    Added 4 test methods into ﻿Task1.Tests.

--- Task1.Tests/ProgramTests.cs
+++ Task1.Tests/ProgramTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using Task1;
 
 namespace Task1.Tests
@@ -11,6 +12,46 @@ namespace Task1.Tests
         {
             int[] numbers = new int[] { 1, 2, 2, 2, 2, 5, 5, 6, 7, 8 };
             int expected = 2;
+
+            int actual = Program.FindFrequentElement(numbers);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindFrequentElement_ArrayWithOneElement1_Returns1()
+        {
+            int[] numbers = new int[] { 1 };
+            int expected = 1;
+
+            int actual = Program.FindFrequentElement(numbers);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindFrequentElement_PassNullParameter_ThrowsException()
+        {
+            int[] numbers = null;
+
+            var ex = Assert.ThrowsException<ArgumentNullException>(() => Program.FindFrequentElement(numbers));
+            StringAssert.Contains(ex.Message, "Value cannot be null. (Parameter 'nums')");
+        }
+
+        [TestMethod]
+        public void FindFrequentElement_PassEmptyParameter_ThrowsException()
+        {
+            int[] numbers = Array.Empty<int>();
+
+            var ex = Assert.ThrowsException<ArgumentException>(() => Program.FindFrequentElement(numbers));
+            StringAssert.Contains(ex.Message, "Array cannot be empty (Parameter 'nums')");
+        }
+
+        [TestMethod]
+        public void FindFrequentElement_ArrayWithNegativeFrequentNumber1_ReturnsMinus1()
+        {
+            int[] numbers = new int[] { -1, -1, -1, -1, -1, 1, 2, 2, 7, 8 };
+            int expected = -1;
 
             int actual = Program.FindFrequentElement(numbers);
 
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -8,6 +8,16 @@ namespace Task1
     {
         public static int FindFrequentElement(int[] nums)
         {
+            if (nums == null)
+            {
+                throw new ArgumentNullException(nameof(nums));
+            }
+
+            if (nums.Length == 0)
+            {
+                throw new ArgumentException("Array cannot be empty", nameof(nums));
+            }
+
             Dictionary<int, int> frequentNumbers = new Dictionary<int, int>();
 
             for (int i = 0; i < nums.Length; i++)
@@ -21,12 +31,13 @@ namespace Task1
                     frequentNumbers.Add(nums[i], 1);
                 }
             }
+
             return frequentNumbers.SingleOrDefault(pair => pair.Value == frequentNumbers.Values.Max()).Key;
         }
+
         static void Main(string[] args)
         {
             int[] numbers = new int[] { 1, 2, 2, 2, 2, 5, 5, 6, 7, 8 };
-            
 
             Console.WriteLine(FindFrequentElement(numbers));
         }
