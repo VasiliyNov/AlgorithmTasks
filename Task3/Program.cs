b7acf8d543d87a359dfd0cf78e460389aab848d5
Vasiliy <vakawelli@gmail.com>
2022-04-25 19:03:14 +0300

    Added a test class ProgramTests with 4 test methods for the unit testing of the Task3.Program.SortPairedUnpairedNumbers() method and some changes in SortPairedUnpairedNumbers() method.

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,39 +4,51 @@ using System.Linq;
 
 namespace Task3
 {
-    class Program
+    public class Program
     {
-        private static void SortPairedUnpairedNumbers(int[] mas)
+        public static void SortPairedUnpairedNumbers(int[] mas)
         {
-            Stack<int> paired = new();
-            Stack<int> unpaired = new();
-            List<int> result = new();
+            if (mas == null)
+            {
+                throw new ArgumentNullException(nameof(mas));
+            }
 
-            for (int i = 0; i < mas.Length; i++)
+            if (mas.Length > 1)
             {
-                if (mas[i] % 2 == 0)
+                Stack<int> paired = new();
+                Stack<int> unpaired = new();
+
+                for (int i = 0; i < mas.Length; i++)
                 {
-                    paired.Push(mas[i]);
+                    if (mas[i] % 2 == 0)
+                    {
+                        paired.Push(mas[i]);
+                    }
+                    else
+                    {
+                        unpaired.Push(mas[i]);
+                    }
                 }
-                else
+
+                int j = 0;
+
+                while (unpaired.Any())
                 {
-                    unpaired.Push(mas[i]);
+                    mas[j++] = paired.Pop();
+                    mas[j++] = unpaired.Pop();
                 }
-            }
-            while (unpaired.Any())
-            {
-                result.Add(paired.Pop());
-                result.Add(unpaired.Pop());
-            }
-            for (int i = 0; i < result.Count; i++)
-            {
-                Console.WriteLine($"nums[{i}]: {result[i]}");
             }
         }
         static void Main(string[] args)
         {
             int[] numbers1 = new int[] { 0, 1, 3, 5, 7, 4, 6, 8, 10, 11, 12, 12, 12, 12, 0, 0, 1, 1, 2, 2, 7, 7, 7, 7, 7, 7 };
+
             SortPairedUnpairedNumbers(numbers1);
+
+            for (int i = 0; i < numbers1.Length; i++)
+            {
+                Console.WriteLine($"nums[{i}]: {numbers1[i]}");
+            }
         }
     }
 }
